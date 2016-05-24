67a8014420c5360a7c40ee9127d1fa8aaf303585
Jake Humphrey <jake@codelation.com>
2016-05-23 21:51:24 -0500

    Adding initial stuff for offline mode

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,9 +5,16 @@ class ContactsController < ApplicationController
   end
 
   def create
-    unless params["contact"].nil?
-      responce = RestClient.post("#{ENV['API_URL']}/contacts", {api_token: ENV["APIKEY"], auth_token: get_user, contact: params["contact"]})
-      json_res = JSON.parse(responce)
+    if params["contact"] && !params.dig("contact","name").empty?
+      c = Contact.new(params["contact"])
+      c.touch
+      response = RestClient.post("#{ENV['API_URL']}/user/contacts", {api_token: ENV["APIKEY"], auth_token: get_user, contact: c.to_json})
+    elsif params["smart"]
+      response = RestClient.post("#{ENV['API_URL']}/user/smarttext", {api_token: ENV["APIKEY"], auth_token: get_user, smarttext: params["smart"]})
+    end
+
+    if response
+      json_res = JSON.parse(response)
       c = Contact.new(json_res)
       logger.ap json_res
       redirect_to contacts_path
@@ -17,14 +24,14 @@ class ContactsController < ApplicationController
   end
 
   def index
-    res = RestClient.get("#{ENV['API_URL']}/contacts", params: {api_token: ENV["APIKEY"], auth_token: get_user})
+    res = RestClient.get("#{ENV['API_URL']}/user/contacts", params: {api_token: ENV["APIKEY"], auth_token: get_user})
     json_res = JSON.parse(res)
     logger.ap json_res
     hash_to_contacts(json_res["data"])
   end
 
   def show
-    res = RestClient.get("#{ENV['API_URL']}/contact/#{params[:id]}", params: {api_token: ENV["APIKEY"], auth_token: get_user})
+    res = RestClient.get("#{ENV['API_URL']}/user/contact/#{params[:id]}", params: {api_token: ENV["APIKEY"], auth_token: get_user})
     @contact = Contact.new(JSON.parse(res))
   end
 
